98b64cbc196c733a728d620c6c5ea8612b9ce3c2
nolotbublika <161640172+nolotbublika@users.noreply.github.com>
2024-04-05 22:36:10 +0300

    Add files via upload

---
+++ Наследование классов..py
@@ -0,0 +1,22 @@
+class Car:
+    price = 1000000
+
+    def horse_power(self):
+
+        print('количество лошидиных сил для автомобиля')
+
+
+class Nissan(Car):
+    price = 1500000
+
+    def horse_power(self):
+
+        print('количество лошидиных сил для автомобиля')
+        
+
+class Kia(Car):
+    price = 200000
+
+    def horse_power(self):
+
+        print('количество лошидиных сил для автомобиля')
